f9ad6825cb32545665d6bf2220ed4d4b5cc86d0d
Mert Cobanov <mertcobanov@gmail.com>
2024-04-26 04:35:03 +0300

    Add translator.py for language translation feature

---
+++ translator.py
@@ -0,0 +1,19 @@
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOllama
+
+
+def setup_translator_chain():
+    """Setup the translation chain with a prompt template and ChatOllama."""
+    prompt_template = PromptTemplate(
+        template="""As a professional translator, provide a detailed and comprehensive translation of the provided text into turkish, ensuring that the translation is accurate, coherent, and faithful to the original text.
+
+        "{text}"
+
+        DETAILED TRANSLATION:""",
+        input_variables=["text"],
+    )
+
+    llm = ChatOllama(model="llama3")
+    llm_chain = LLMChain(llm=llm, prompt=prompt_template)
+    return llm_chain
